5a64fc9c132bccae60d6cf8fa22c8fd8da28725c
wursteis@923e5a50-5539-0410-adbe-9b5e3d355a7a <none@none>
2008-02-10 17:12:06 +0000

    first automatic screenshot
    --HG--
    extra : convert_revision : svn%3A923e5a50-5539-0410-adbe-9b5e3d355a7a/trunk%40109

---
+++ musicsvg-site/src/test/java/net/sourceforge/MainScreenshotTest.java
@@ -0,0 +1,35 @@
+package net.sourceforge;
+
+import java.io.File;
+import java.io.IOException;
+import net.sourceforge.musicsvg.main.Main;
+import org.fest.swing.fixture.FrameFixture;
+import org.fest.swing.util.ScreenshotTaker;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class MainScreenshotTest {
+    private String imageFolderPath;
+
+    private FrameFixture gui;
+    private ScreenshotTaker screenshotTaker = new ScreenshotTaker();
+
+    @BeforeMethod
+    public void setUp() throws IOException {
+
+        String currentFolderPath = org.fest.util.Files.currentFolder().getCanonicalPath();
+
+        File imageFolder = new File(currentFolderPath);
+        imageFolderPath = imageFolder.getCanonicalPath() + File.separator;
+
+        Main mainApp = new Main();
+        gui = new FrameFixture(mainApp.startApplication());
+        gui.show();
+    }
+
+    @Test
+    public void testMainFrame() {
+        screenshotTaker.saveDesktopAsPng(imageFolderPath + "mainFrame.png");
+
+    }
+}
