5b64f3838dec37530ece98d0a51df8f8b7bb6633
wursteis@923e5a50-5539-0410-adbe-9b5e3d355a7a <none@none>
2008-02-19 21:01:56 +0000

    module
    --HG--
    extra : convert_revision : svn%3A923e5a50-5539-0410-adbe-9b5e3d355a7a/trunk%40136

--- musicsvg-utils/src/main/java/net/sourceforge/musicsvg/utils/module/UtilsModule.java
+++ musicsvg-utils/src/main/java/net/sourceforge/musicsvg/utils/module/UtilsModule.java
@@ -2,14 +2,14 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package net.sourceforge.musicsvg.utils.module;
 
 import com.google.inject.Binder;
 import com.google.inject.Module;
 import com.google.inject.Scopes;
-import java.util.ResourceBundle;
+import net.sourceforge.musicsvg.utils.I18n;
 import net.sourceforge.musicsvg.utils.MusicSVGLogger;
+import net.sourceforge.musicsvg.utils.impl.I18nImpl;
 import net.sourceforge.musicsvg.utils.impl.MusicSVGLoggerLog4jImpl;
 
 /**
@@ -22,10 +22,6 @@ public class UtilsModule implements Module {
     public void configure(Binder binder) {
         // Logger
         binder.bind(MusicSVGLogger.class).to(MusicSVGLoggerLog4jImpl.class).in(Scopes.SINGLETON);
-
-        // Bundle
-        ResourceBundle resource = ResourceBundle.getBundle("locales/Music");
-        binder.bind(ResourceBundle.class).toInstance(resource);
+        binder.bind(I18n.class).to(I18nImpl.class).in(Scopes.SINGLETON);
     }
-
 }
