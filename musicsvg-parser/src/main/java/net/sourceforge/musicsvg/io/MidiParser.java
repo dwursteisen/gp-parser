34a065e1d9c745ee949973598f1d805a4207775a
wursteis@923e5a50-5539-0410-adbe-9b5e3d355a7a <none@none>
2008-05-10 14:05:19 +0000

    add jfugue and new parser
    --HG--
    extra : convert_revision : svn%3A923e5a50-5539-0410-adbe-9b5e3d355a7a/trunk%40166

---
+++ musicsvg-parser/src/main/java/net/sourceforge/musicsvg/io/MidiParser.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.sourceforge.musicsvg.io;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author Dav
+ */
+public class MidiParser {
+
+    public void openFile(File file) throws IOException {
+        
+    }
+            
+}
