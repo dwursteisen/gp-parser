d74b419609f9fd5c829309dd3349c5c2522062c3
wursteis@923e5a50-5539-0410-adbe-9b5e3d355a7a <none@none>
2008-06-30 21:55:01 +0000

    drawing of the componant
    --HG--
    extra : convert_revision : svn%3A923e5a50-5539-0410-adbe-9b5e3d355a7a/trunk%40172

--- musicsvg-drums-gui/src/main/java/net/sourceforge/musicsvg/drums/gui/DrumPadComponent.java
+++ musicsvg-drums-gui/src/main/java/net/sourceforge/musicsvg/drums/gui/DrumPadComponent.java
@@ -5,6 +5,7 @@
 
 package net.sourceforge.musicsvg.drums.gui;
 
+import java.awt.Graphics;
 import javax.swing.JComponent;
 
 /**
@@ -19,8 +20,27 @@ public class DrumPadComponent extends JComponent {
     final static int DRUM_PAD_WITH = 8;
     final static int DRUM_PAD_HEIGHT = 8;
     
-    
     public void push(final int x, final int y) {
         drumPadMatrix[x][y] ^= BUTTON_ACTIVE;
     }
+    
+    
+    public void paintPad(final Graphics g, final int x, final int y, final byte state) {
+        int width = 20;
+        int height = 20;
+        int space = 5;
+        g.drawRect(x*(width+space), y*(height+space), width, height);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+    
+        for(int x = 0; x < DRUM_PAD_WITH ; x++) {
+            for(int y = 0 ; y < DRUM_PAD_HEIGHT ; y++) {
+                paintPad(g, x, y, drumPadMatrix[x][y]);
+            }
+        }
+    }
+    
+    
 }
