e17b5ac8c923beebf194442b2907c1427dddac96
wursteis@923e5a50-5539-0410-adbe-9b5e3d355a7a <none@none>
2009-04-10 22:01:35 +0000

    now launch guitar pro
    --HG--
    extra : convert_revision : svn%3A923e5a50-5539-0410-adbe-9b5e3d355a7a/trunk%40191

--- musicsvg-librarie/src/main/java/net/sourceforge/musicsvg/gui/LibrarieMainPanel.java
+++ musicsvg-librarie/src/main/java/net/sourceforge/musicsvg/gui/LibrarieMainPanel.java
@@ -4,6 +4,9 @@
  */
 package net.sourceforge.musicsvg.gui;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -13,6 +16,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.table.AbstractTableModel;
+import net.sourceforge.musicsvg.model.Song;
 import org.apache.log4j.Logger;
 
 /**
@@ -32,6 +36,26 @@ public class LibrarieMainPanel extends JPanel {
         jTable = new javax.swing.JTable();
         jTable.setModel(model);
         jTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        jTable.addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                JTable table;
+                if(e.getClickCount() >= 2) {
+                    
+                    table = (JTable)e.getSource();
+                    SongTableModel model = (SongTableModel) table.getModel();
+                    Song s = model.getItemsAt(table.getSelectedRow());
+                    LOG.debug("Lancement de Guitar Pro sur le ficiher..."+s.getFile().getAbsolutePath());
+                    try {
+                        Runtime.getRuntime().exec("C:\\Program Files\\Guitar Pro 5\\GP5.exe " + s.getFile().getAbsolutePath());
+                    } catch (IOException ex) {
+                        LOG.error("Erreur lors du lancement de Guitar Pro", ex);
+                    }
+                }
+            }
+
+        });
         jScrollPane2.setViewportView(jTable);
 
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
