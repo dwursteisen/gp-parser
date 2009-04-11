e9ac2e5bf3052efc78f9b3dd06d333b5c121a24b
wursteis@923e5a50-5539-0410-adbe-9b5e3d355a7a <none@none>
2009-04-11 17:22:17 +0000

    filter on the enter key
    --HG--
    extra : convert_revision : svn%3A923e5a50-5539-0410-adbe-9b5e3d355a7a/trunk%40198

--- musicsvg-librarie/src/main/java/net/sourceforge/musicsvg/gui/SearchPanel.java
+++ musicsvg-librarie/src/main/java/net/sourceforge/musicsvg/gui/SearchPanel.java
@@ -2,11 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package net.sourceforge.musicsvg.gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -19,30 +20,44 @@ import net.sourceforge.musicsvg.gui.listener.FilterListener;
  * @author Dav
  */
 public class SearchPanel extends JPanel {
+
     private JTextField searchField;
     private JLabel searchLabel;
     private JButton searchButton;
+    private List<FilterListener> filterListeners;
 
     public SearchPanel() {
         searchLabel = new JLabel("Filtre...");
         searchField = new JTextField(40);
         searchButton = new JButton("OK");
-        
+
         add(searchLabel);
         add(searchField);
         add(searchButton);
     }
 
     public void setOnFilterListeners(final List<FilterListener> listeners) {
+        this.filterListeners = listeners;
         searchButton.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent e) {
-                for(FilterListener l : listeners) {
-                    l.filter(searchField.getText());
+                fireFilterEvent();
+            }
+        });
+
+        searchField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    fireFilterEvent();
                 }
             }
         });
-        
     }
 
+    private void fireFilterEvent() {
+        for (FilterListener l : filterListeners) {
+            l.filter(searchField.getText());
+        }
+    }
 }
