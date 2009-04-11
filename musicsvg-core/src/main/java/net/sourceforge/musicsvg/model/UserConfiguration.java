275c090f80fce3614be83eb6ab7af154b48bc50d
wursteis@923e5a50-5539-0410-adbe-9b5e3d355a7a <none@none>
2009-04-11 15:01:48 +0000

    remove unused import
    --HG--
    extra : convert_revision : svn%3A923e5a50-5539-0410-adbe-9b5e3d355a7a/trunk%40197

--- musicsvg-core/src/main/java/net/sourceforge/musicsvg/model/UserConfiguration.java
+++ musicsvg-core/src/main/java/net/sourceforge/musicsvg/model/UserConfiguration.java
@@ -7,8 +7,6 @@ package net.sourceforge.musicsvg.model;
 import java.io.File;
 import java.io.Serializable;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
