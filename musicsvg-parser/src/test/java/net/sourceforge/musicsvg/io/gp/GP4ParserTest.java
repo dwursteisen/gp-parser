9dcc58346f0bcd4eea06b326f2907499e1dd7e19
dwursteisen <david.wursteisen+git@gmail.com>
2012-09-02 18:41:20 +0200

    Update dependencies - use lastest testng - use mockito instead of easymock - use fest-assert

--- musicsvg-parser/src/test/java/net/sourceforge/musicsvg/io/gp/GP4ParserTest.java
+++ musicsvg-parser/src/test/java/net/sourceforge/musicsvg/io/gp/GP4ParserTest.java
@@ -7,16 +7,17 @@
 package net.sourceforge.musicsvg.io.gp;
 
 import net.sourceforge.musicsvg.io.gp.listeners.GP4ParserListener;
-import java.io.File;
-import org.easymock.EasyMock;
-import org.easymock.IMocksControl;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 /**
- *
  * @author Dav
  */
 public class GP4ParserTest {
@@ -26,19 +27,25 @@ public class GP4ParserTest {
     public static final String SOLFEGE_FILE = "/gp4/solfege.gp4";
     public static final String BLUES_FILE = "/gp4/rythme_blues.gp4";
     public static final String RYTHME_FILE = "/gp4/rythme.gp4";
-    
+
     private File simpleFile;
     private File solfegeFile;
     private File bluesFile;
     private File rythmeFile;
 
+    @Mock
+    private GP4ParserListener listener;
+
     @BeforeMethod
     public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this); //This could be pulled up into a shared base class
+
         simpleFile = new File(this.getClass().getResource(SIMPLE_FILE).toURI());
         bluesFile = new File(this.getClass().getResource(BLUES_FILE).toURI());
         solfegeFile = new File(this.getClass().getResource(SOLFEGE_FILE).toURI());
         rythmeFile = new File(this.getClass().getResource(RYTHME_FILE).toURI());
         instance = new GP4Parser();
+        instance.setListener(listener);
     }
 
     @AfterMethod
@@ -48,169 +55,109 @@ public class GP4ParserTest {
 
     @Test
     public void testReadFileHeaders() throws Exception {
-        IMocksControl control = EasyMock.createNiceControl();
-        GP4ParserListener listener = control.createMock(GP4ParserListener.class);
-        listener.readVersion("FICHIER GUITAR PRO v4.06");
-        listener.readTitle("title");
-        listener.readSubTitle("subtitle");
-        listener.readArtist("artist");
-        listener.readAlbum("album");
-        listener.readSongAuthor("music");
-        listener.readCopyright("copyright");
-        // listener.readPieceAuthor(String pieceAuthor);
-        listener.readInstruction("instructions");
-        listener.readNumberOfNotices(1);
-        listener.readNotice("notice");
-        listener.readIsTripleFeel(false);
-        // listener.readTempoValue(120);
-
-        EasyMock.replay(listener);
-
-        instance.setListener(listener);
-
         instance.openFile(simpleFile);
 
-        control.verify();
+        // verify
+        Mockito.verify(listener).readVersion("FICHIER GUITAR PRO v4.06");
+        Mockito.verify(listener).readTitle("title");
+        Mockito.verify(listener).readSubTitle("subtitle");
+        Mockito.verify(listener).readArtist("artist");
+        Mockito.verify(listener).readAlbum("album");
+        Mockito.verify(listener).readSongAuthor("music");
+        Mockito.verify(listener).readCopyright("copyright");
+        // listener.readPieceAuthor(String pieceAuthor);
+        Mockito.verify(listener).readInstruction("instructions");
+        Mockito.verify(listener).readNumberOfNotices(1);
+        Mockito.verify(listener).readNotice("notice");
+        Mockito.verify(listener).readIsTripleFeel(false);
+        // listener.readTempoValue(120);
 
     }
 
     @Test
     public void testReadOtherInformations() throws Exception {
 
-        IMocksControl control = EasyMock.createNiceControl();
-        GP4ParserListener listener = control.createMock(GP4ParserListener.class);
-        listener.readTempoValue(120);
+        instance.openFile(simpleFile);
+
+        // verify
+        Mockito.verify(listener).readTempoValue(120);
         /**
          *  Bb (bb)= -2
          *  F (b)  = -1
          *  C      = 0
-         *  G (#)  = 1  
+         *  G (#)  = 1
          */
-        listener.readKey(0);
-        listener.readOctave(0); // not used by guitar pro
+        Mockito.verify(listener).readKey(0);
+        Mockito.verify(listener).readOctave(0); // not used by guitar pro
         // listener.readChannel(channel, effectChannel, instrument, volume, balance, chorus, reverb, phaser, tremolo, solo, mute)
 
-        listener.readMidiChannel(0, 0, 25, (byte) 7, (byte) 3, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
-        listener.readMidiChannel(0, 1, 25, (byte) 7, (byte) 3, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
-        listener.readNumberOfMesures(3);
-        listener.readNumberOfTracks(1);
-
-        EasyMock.replay(listener);
-
-        instance.setListener(listener);
-
-        instance.openFile(simpleFile);
-
-        control.verify();
+        Mockito.verify(listener).readMidiChannel(0, 0, 25, (byte) 7, (byte) 3, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
+        Mockito.verify(listener).readMidiChannel(0, 1, 25, (byte) 7, (byte) 3, (byte) 0, (byte) 0, (byte) 0, (byte) 0);
+        Mockito.verify(listener).readNumberOfMesures(3);
+        Mockito.verify(listener).readNumberOfTracks(1);
 
     }
 
     @Test
     public void testReadMesureHeader() throws Exception {
-        IMocksControl control = EasyMock.createNiceControl();
-        GP4ParserListener listener = control.createMock(GP4ParserListener.class);
-        listener.readMesureMarker(0, "Section 1", 255, 0, 0);
-        listener.readMeasureTonality(0);
-        listener.readMeasureHeader(0, 4, 4, false, false, 0, 0);
-        listener.readMeasureHeader(1, 0, 0, false, false, 0, 0);
-        listener.readMeasureHeader(2, 3, 8, false, false, 0, 0);
 
-        EasyMock.replay(listener);
-
-        instance.setListener(listener);
 
         instance.openFile(simpleFile);
 
-        control.verify();
+        Mockito.verify(listener).readMesureMarker(0, "Section 1", 255, 0, 0);
+        Mockito.verify(listener).readMeasureTonality(0);
+        Mockito.verify(listener).readMeasureHeader(0, 4, 4, false, false, 0, 0);
+        Mockito.verify(listener).readMeasureHeader(1, 0, 0, false, false, 0, 0);
+        Mockito.verify(listener).readMeasureHeader(2, 3, 8, false, false, 0, 0);
 
     }
 
     @Test
     public void testReadTrack() throws Exception {
-        IMocksControl control = EasyMock.createNiceControl();
-        GP4ParserListener listener = control.createMock(GP4ParserListener.class);
-        listener.readTrackParameter(0, "Track 1", 6, false, false, false);
 
-        EasyMock.replay(listener);
-
-        instance.setListener(listener);
 
         instance.openFile(simpleFile);
 
-        control.verify();
+        Mockito.verify(listener).readTrackParameter(0, "Track 1", 6, false, false, false);
 
     }
 
     @Test
     public void testReadStringTunning() throws Exception {
-        IMocksControl control = EasyMock.createNiceControl();
-        GP4ParserListener listener = control.createMock(GP4ParserListener.class);
-        listener.readStringTunning(0, 0, 64);
-        listener.readStringTunning(0, 1, 59);
-        listener.readStringTunning(0, 2, 55);
-        listener.readStringTunning(0, 3, 50);
-        listener.readStringTunning(0, 4, 45);
-        listener.readStringTunning(0, 5, 40);
-
-        EasyMock.replay(listener);
-
-        instance.setListener(listener);
 
         instance.openFile(simpleFile);
-
-        control.verify();
+        Mockito.verify(listener).readStringTunning(0, 0, 64);
+        Mockito.verify(listener).readStringTunning(0, 1, 59);
+        Mockito.verify(listener).readStringTunning(0, 2, 55);
+        Mockito.verify(listener).readStringTunning(0, 3, 50);
+        Mockito.verify(listener).readStringTunning(0, 4, 45);
+        Mockito.verify(listener).readStringTunning(0, 5, 40);
 
     }
 
     @Test
     public void testReadTrackMidiParameter() throws Exception {
-        IMocksControl control = EasyMock.createNiceControl();
-        GP4ParserListener listener = control.createMock(GP4ParserListener.class);
-        listener.readTrackMidiParameter(0, 1, 1, 2, 24, 0, 255, 0, 0);
-
-        EasyMock.replay(listener);
-
-        instance.setListener(listener);
-
         instance.openFile(simpleFile);
-
-        control.verify();
-
+        Mockito.verify(listener).readTrackMidiParameter(0, 1, 1, 2, 24, 0, 255, 0, 0);
     }
 
     @Test
     public void testReadNumberOfBeats() throws Exception {
-        IMocksControl control = EasyMock.createNiceControl();
-        GP4ParserListener listener = control.createMock(GP4ParserListener.class);
-        listener.readNumberOfBeats(0, 0, 4);
-        listener.readNumberOfBeats(0, 1, 1); // this beat can be empty
-
-        listener.readNumberOfBeats(0, 2, 1);
-
-        EasyMock.replay(listener);
-
-        instance.setListener(listener);
-
         instance.openFile(simpleFile);
+        Mockito.verify(listener).readNumberOfBeats(0, 0, 4);
+        Mockito.verify(listener).readNumberOfBeats(0, 1, 1); // this beat can be empty
 
-        control.verify();
+        Mockito.verify(listener).readNumberOfBeats(0, 2, 1);
 
     }
 
     @Test
     public void testReadEmtpyBeat() throws Exception {
-        IMocksControl control = EasyMock.createNiceControl();
-        GP4ParserListener listener = control.createMock(GP4ParserListener.class);
-        listener.readEmptyBeat(0, 1, 0, true, false);
-        listener.readEmptyBeat(0, 2, 0, true, false);
-
-        EasyMock.replay(listener);
-
-        instance.setListener(listener);
 
         instance.openFile(simpleFile);
 
-        control.verify();
+        Mockito.verify(listener).readEmptyBeat(0, 1, 0, true, false);
+        Mockito.verify(listener).readEmptyBeat(0, 2, 0, true, false);
 
     }
 
@@ -225,71 +172,37 @@ public class GP4ParserTest {
          * are set to 1
          *    0001100 for example
          */
-        IMocksControl control = EasyMock.createNiceControl();
-        GP4ParserListener listener = control.createMock(GP4ParserListener.class);
-        listener.readStringPlayed(0, 0, 0, 0xC);
-        listener.readStringPlayed(0, 0, 1, 0x4);
-        listener.readStringPlayed(0, 0, 2, 0x4);
-        listener.readStringPlayed(0, 0, 3, 0x4);
-        listener.readStringPlayed(0, 1, 0, 0x0); // silence
-
-        EasyMock.replay(listener);
-
-        instance.setListener(listener);
-
         instance.openFile(simpleFile);
-
-        control.verify();
+        Mockito.verify(listener).readStringPlayed(0, 0, 0, 0xC);
+        Mockito.verify(listener).readStringPlayed(0, 0, 1, 0x4);
+        Mockito.verify(listener).readStringPlayed(0, 0, 2, 0x4);
+        Mockito.verify(listener).readStringPlayed(0, 0, 3, 0x4);
+        Mockito.verify(listener).readStringPlayed(0, 1, 0, 0x0); // silence
 
     }
 
     @Test
     public void testReadNote() throws Exception {
-        IMocksControl control = EasyMock.createNiceControl();
-        // control.checkOrder(true);
-        GP4ParserListener listener = control.createMock(GP4ParserListener.class);
-
-        // First mesure ( 0  on each string)
-        listener.readNote(0, 0, 0, 3, (byte) 5, 0);
-        listener.readNote(0, 0, 0, 2, (byte) 3, 0);
-        listener.readNote(0, 0, 1, 2, (byte) 5, 0);
-        listener.readNote(0, 0, 2, 2, (byte) 7, 0);
-        listener.readNote(0, 0, 3, 2, (byte) 8, 0);
-
-        EasyMock.replay(listener);
-
-        instance.setListener(listener);
-
         instance.openFile(simpleFile);
-
-        control.verify();
-
+        // First mesure ( 0  on each string)
+        Mockito.verify(listener).readNote(0, 0, 0, 3, (byte) 5, 0);
+        Mockito.verify(listener).readNote(0, 0, 0, 2, (byte) 3, 0);
+        Mockito.verify(listener).readNote(0, 0, 1, 2, (byte) 5, 0);
+        Mockito.verify(listener).readNote(0, 0, 2, 2, (byte) 7, 0);
+        Mockito.verify(listener).readNote(0, 0, 3, 2, (byte) 8, 0);
     }
-    
+
     @Test
     public void testReadBeat() throws Exception {
-        System.out.println("readBeat");
-        IMocksControl control = EasyMock.createNiceControl();
-        // control.checkOrder(true);
-        GP4ParserListener listener = control.createMock(GP4ParserListener.class);
-
-        // First mesure ( 0  on each string)
-        listener.readBeat(0, 0, 0, -1, false);
-        listener.readBeat(0, 0, 1, 0, false);
-        listener.readBeat(0, 0, 2, 1, false);
-        listener.readBeat(0, 0, 3, 2, false);
-        listener.readBeat(0, 0, 4, 2, false);
-        
-        EasyMock.replay(listener);
-
-        instance.setListener(listener);
-
         instance.openFile(rythmeFile);
-
-        control.verify();
-
+        // First mesure ( 0  on each string)
+        Mockito.verify(listener).readBeat(0, 0, 0, -1, false);
+        Mockito.verify(listener).readBeat(0, 0, 1, 0, false);
+        Mockito.verify(listener).readBeat(0, 0, 2, 1, false);
+        Mockito.verify(listener).readBeat(0, 0, 3, 2, false);
+        Mockito.verify(listener).readBeat(0, 0, 4, 2, false);
     }
-    
+
 
     @Test
     public void testNbStringPlayed() {
@@ -363,82 +276,64 @@ public class GP4ParserTest {
 
     @Test
     public void testReadNote_blues_file() throws Exception {
-        IMocksControl control = EasyMock.createNiceControl();
-        // control.checkOrder(true);
-        GP4ParserListener listener = control.createMock(GP4ParserListener.class);
-        listener.readNumberOfTracks(3);
-        listener.readNumberOfMesures(20);
-        EasyMock.replay(listener);
-
-        instance.setListener(listener);
-
         instance.openFile(bluesFile);
 
-        EasyMock.verify(listener);
+        listener.readNumberOfTracks(3);
+        listener.readNumberOfMesures(20);
 
     }
 
     @Test
     public void testReadNote_solfege_file() throws Exception {
-        IMocksControl control = EasyMock.createNiceControl();
-        // control.checkOrder(true);
-        GP4ParserListener listener = control.createMock(GP4ParserListener.class);
-
+        instance.openFile(solfegeFile);
         // First mesure ( 0  on each string)
-        listener.readNote(0, 0, 0, 1, (byte) 0, 0);
-        listener.readNote(0, 0, 1, 2, (byte) 0, 0);
-        listener.readNote(0, 0, 2, 3, (byte) 0, 0);
-        listener.readNote(0, 0, 3, 4, (byte) 0, 0);
-        listener.readNote(0, 0, 4, 5, (byte) 0, 0);
-        listener.readNote(0, 0, 5, 6, (byte) 0, 0);
-        listener.readNote(0, 0, 6, 5, (byte) 0, 0);
-        listener.readNote(0, 0, 7, 4, (byte) 0, 0);
+        Mockito.verify(listener).readNote(0, 0, 0, 1, (byte) 0, 0);
+        Mockito.verify(listener).readNote(0, 0, 1, 2, (byte) 0, 0);
+        Mockito.verify(listener).readNote(0, 0, 2, 3, (byte) 0, 0);
+        Mockito.verify(listener).readNote(0, 0, 3, 4, (byte) 0, 0);
+        Mockito.verify(listener).readNote(0, 0, 4, 5, (byte) 0, 0);
+        Mockito.verify(listener).readNote(0, 0, 5, 6, (byte) 0, 0);
+        Mockito.verify(listener).readNote(0, 0, 6, 5, (byte) 0, 0);
+        Mockito.verify(listener).readNote(0, 0, 7, 4, (byte) 0, 0);
 
         // second mesure (1, 2, 3, on each string)
-        listener.readNote(0, 1, 0, 1, (byte) 1, 0);
-        listener.readNote(0, 1, 1, 2, (byte) 2, 0);
-        listener.readNote(0, 1, 2, 3, (byte) 3, 0);
-        listener.readNote(0, 1, 3, 4, (byte) 4, 0);
-        listener.readNote(0, 1, 4, 5, (byte) 5, 0);
-        listener.readNote(0, 1, 5, 6, (byte) 6, 0);
-        listener.readNote(0, 1, 6, 5, (byte) 7, 0);
-        listener.readNote(0, 1, 7, 4, (byte) 8, 0);
+        Mockito.verify(listener).readNote(0, 1, 0, 1, (byte) 1, 0);
+        Mockito.verify(listener).readNote(0, 1, 1, 2, (byte) 2, 0);
+        Mockito.verify(listener).readNote(0, 1, 2, 3, (byte) 3, 0);
+        Mockito.verify(listener).readNote(0, 1, 3, 4, (byte) 4, 0);
+        Mockito.verify(listener).readNote(0, 1, 4, 5, (byte) 5, 0);
+        Mockito.verify(listener).readNote(0, 1, 5, 6, (byte) 6, 0);
+        Mockito.verify(listener).readNote(0, 1, 6, 5, (byte) 7, 0);
+        Mockito.verify(listener).readNote(0, 1, 7, 4, (byte) 8, 0);
 
         // third mesure (chord)
-        listener.readNote(0, 2, 0, 2, (byte) 2, 0);
-        listener.readNote(0, 2, 0, 1, (byte) 0, 0);
+        Mockito.verify(listener).readNote(0, 2, 0, 2, (byte) 2, 0);
+        Mockito.verify(listener).readNote(0, 2, 0, 1, (byte) 0, 0);
 
-        listener.readNote(0, 2, 1, 3, (byte) 2, 0);
-        listener.readNote(0, 2, 1, 2, (byte) 0, 0);
+        Mockito.verify(listener).readNote(0, 2, 1, 3, (byte) 2, 0);
+        Mockito.verify(listener).readNote(0, 2, 1, 2, (byte) 0, 0);
 
-        listener.readNote(0, 2, 2, 4, (byte) 2, 0);
-        listener.readNote(0, 2, 2, 3, (byte) 0, 0);
+        Mockito.verify(listener).readNote(0, 2, 2, 4, (byte) 2, 0);
+        Mockito.verify(listener).readNote(0, 2, 2, 3, (byte) 0, 0);
 
-        listener.readNote(0, 2, 3, 5, (byte) 2, 0);
-        listener.readNote(0, 2, 3, 4, (byte) 0, 0);
+        Mockito.verify(listener).readNote(0, 2, 3, 5, (byte) 2, 0);
+        Mockito.verify(listener).readNote(0, 2, 3, 4, (byte) 0, 0);
 
-        listener.readNote(0, 2, 4, 6, (byte) 2, 0);
-        listener.readNote(0, 2, 4, 5, (byte) 0, 0);
+        Mockito.verify(listener).readNote(0, 2, 4, 6, (byte) 2, 0);
+        Mockito.verify(listener).readNote(0, 2, 4, 5, (byte) 0, 0);
 
-        listener.readNote(0, 2, 5, 3, (byte) 5, 0);
-        listener.readNote(0, 2, 5, 2, (byte) 5, 0);
-        listener.readNote(0, 2, 5, 1, (byte) 3, 0);
+        Mockito.verify(listener).readNote(0, 2, 5, 3, (byte) 5, 0);
+        Mockito.verify(listener).readNote(0, 2, 5, 2, (byte) 5, 0);
+        Mockito.verify(listener).readNote(0, 2, 5, 1, (byte) 3, 0);
 
-        listener.readNote(0, 2, 6, 4, (byte) 5, 0);
-        listener.readNote(0, 2, 6, 3, (byte) 5, 0);
-        listener.readNote(0, 2, 6, 2, (byte) 3, 0);
+        Mockito.verify(listener).readNote(0, 2, 6, 4, (byte) 5, 0);
+        Mockito.verify(listener).readNote(0, 2, 6, 3, (byte) 5, 0);
+        Mockito.verify(listener).readNote(0, 2, 6, 2, (byte) 3, 0);
 
-        listener.readNote(0, 2, 7, 5, (byte) 5, 0);
-        listener.readNote(0, 2, 7, 4, (byte) 5, 0);
-        listener.readNote(0, 2, 7, 3, (byte) 3, 0);
+        Mockito.verify(listener).readNote(0, 2, 7, 5, (byte) 5, 0);
+        Mockito.verify(listener).readNote(0, 2, 7, 4, (byte) 5, 0);
+        Mockito.verify(listener).readNote(0, 2, 7, 3, (byte) 3, 0);
 
-        EasyMock.replay(listener);
-
-        instance.setListener(listener);
-
-        instance.openFile(solfegeFile);
-
-        control.verify();
 
     }
 
